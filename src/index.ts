9457c50a8bb41d212a4763ba24a05bc86cce31e2
Sigrid <sigrid.huemer@posteo.at>
2020-07-04 20:36:44 +0200

    0.2.1
    d

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export { perlin2D } from "./noiseFunctions/perlin2D"
-export { perlin3D } from "./noiseFunctions/perlin3D"
+export { perlin2D } from "./noiseFunctions/perlin2D";
+export { perlin3D } from "./noiseFunctions/perlin3D";
 export { simplex2D } from "./noiseFunctions/simplex2D";
 export { simplex3D } from "./noiseFunctions/simplex3D";
 export { simplex4D } from "./noiseFunctions/simplex4D";
-
